15d467c83cd685fcf3098caa76960100713ed780
j-morano <research.msj@gmail.com>
2025-07-04 10:31:04 +0200

    Ordered workspaces

--- i3/to_workspace.py
+++ i3/to_workspace.py
@@ -14,16 +14,42 @@ output = focused_workspace.output
 
 
 option = sys.argv[1]
-workspace_id = sys.argv[2]
+workspace_name = sys.argv[2]
 
+out_mapping = {
+    'HDMI-0': '2',
+    'HDMI-1': '1',
+}
 
-out_idd = str(output).split('-')[1]
-workspace_name = f"{out_idd}:{workspace_id}"
-# print(f"Workspace name: {workspace_name}")
+# See i3/config for more info on workspace naming
+ws_mapping = {
+    'x': '00',
+    '1': '01',
+    '2': '02',
+    '3': '03',
+    '4': '04',
+    '5': '05',
+    '6': '06',
+    '7': '07',
+    '8': '08',
+    '9': '09',
+    '0': '10',
+    'n': '11',
+    'm': '12',
+    'g': '13',
+    'u': '14',
+}
+
+out_id = out_mapping.get(output, '1')
+workspace_id = f"{out_id}{ws_mapping[workspace_name]}"
+# Remove leading zeros for workspace ID
+workspace_id = workspace_id.lstrip('0')
+full_workspace_name = f"{workspace_id}:{workspace_name}"
+
 
 if option == 'move':
     # Move the focused window to the specified workspace
-    i3.command(f'move container to workspace "{workspace_name}"')
+    i3.command(f'move container to workspace "{full_workspace_name}"')
 elif option == 'to':
     # Focus the specified workspace
-    i3.command(f'workspace "{workspace_name}"')
+    i3.command(f'workspace "{full_workspace_name}"')
